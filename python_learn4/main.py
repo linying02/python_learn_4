dcd360ebe8f55c8ccb1202277e719be295612cc8
凜樱 <sun52492@qq.com>
2024-09-28 18:01:17 +0800

    Add files via upload
    学习笔记

---
+++ python_learn4/main.py
@@ -0,0 +1,58 @@
+#不可改变的列表->元组，特殊的，如果元组嵌套列表，那么列表中的值是可以改变的
+# tuple_1 = ("山","东","科","技","大","学")
+# num = tuple_1.index("山")
+# print(num)
+# num_1 = tuple_1.count("山")
+# print(num_1)
+# num_2 = len(tuple_1)
+# print(num_2)
+#元组的循环遍历与列表一样
+
+#字符串的定义与使用，不可修改
+# str_1 = " shtdbbdrhh dhdjyjf dhfjf djfj  "
+# print(str_1[2])
+#根据元素查找下标
+# str_1 = " shtdbbdrhh dhdjyjf dhfjf djfj  "
+# num = str_1.index("h")
+# print(num)
+#字符串的替换
+# str_1 = " shtdbbdrhh dhdjyjf dhfjf djfj  "
+# str_2 = str_1.replace("sht","dhd")
+# print(str_2)
+#按照给定的字符串，对字符串进行分隔
+# str_1 = " shtdbbdrhh dhdjyjf dhfjf djfj  "
+# str_2 = str_1.split("h")
+# print(str_2)
+#移除首位的空格和换行符或指定字符串
+# str_1 = " shtdbbdrhh dhdjyjf dhfjf djfj "
+# str_2 = str_1.strip(" ")
+# print(str_2)
+#统计字符串出现的次数
+# str_1 = " shtdbbdrhh dhdjyjf dhfjf djfj "
+# str_2 = str_1.count("h")
+# print(str_2)
+#计算字符串的长度
+# str_1 = " shtdbbdrhh dhdjyjf dhfjf djfj "
+# str_2 = len(str_1)
+# print(str_2)
+#分割字符串
+# str_1 = "itheima itcast boxuegu"
+# str_2 = str_1.count("it")
+# print(str_2)
+# str_3 = str_1.replace(" ","I")
+# print(str_3)
+# str_4 = str_3.split("I")
+# print(str_4)
+#序列的切片操作
+#字符串
+# str_1 = "123456789"
+# num = str_1[::2]
+# print(num)
+#列表
+# list_1 = [1,2,3,4,5,6,7,8,9]
+# num = list_1[::2]
+# print(num)
+#元组
+# touple_1 = (1,2,3,4,5,6,7,8,9)
+# num = touple_1[::2]
+# print(num)
